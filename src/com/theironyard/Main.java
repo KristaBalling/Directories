c4dd2a787270a38ea1d27c0eabeb855f98bca6d2
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-14 13:52:37 -0500

    uses DirectoryStream to filter file tree results

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -2,18 +2,27 @@ package com.theironyard;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-    Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
-    try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
-        for(Path file: contents) {
-            System.out.println(file.getFileName());
+
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path path) throws IOException {
+                return Files.isRegularFile(path);
+            }
+        };
+
+        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
         }
-    } catch(IOException | DirectoryIteratorException e) {
-        System.out.println(e.getMessage());
-    }
 
     }
 }
