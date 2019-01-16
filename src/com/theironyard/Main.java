3d32f37c9c772a350c3e6451ed4d72d04b7f8d9e
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-16 14:17:57 -0500

    copy entire tree

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,7 +3,6 @@ package com.theironyard;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Iterator;
 
 public class Main {
 
@@ -58,6 +57,18 @@ public class Main {
             Files.walkFileTree(dir2Path, new PrintNames());
         } catch(IOException e) {
             System.out.println(e.getMessage());
+        }
+
+        System.out.println("---Copy Dir2 to Dir4/Dir2Copy---");
+        Path copyPath = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir4" + File.separator
+        + "Dir2Copy");
+        // FileTree/Dir4/Dir2Copy
+        try {
+            Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+
         }
     }
 }
